d37520c3796160e00483172e184367c1d2d8db5e
Lagourgue Maxence <maxence.lagourgue@free.fr>
2024-05-21 16:06:37 +0200

    updating fenetre

--- src/main/java/ihm/Fenetre.java
+++ src/main/java/ihm/Fenetre.java
@@ -3,15 +3,19 @@ package ihm;
 import javax.swing.*;
 
 public class Fenetre extends JFrame {
+
     public static void main(String[] args) {
         new Fenetre();
     }
     public Fenetre() {
+        init();
+    }
+    public void init() {
         this.setTitle("Fromagerie");
         this.setSize(1000, 800);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
-        this.setResizable(false);
+        this.setResizable(true);
         this.setContentPane(new JPanel());
         this.setVisible(true);
     }
