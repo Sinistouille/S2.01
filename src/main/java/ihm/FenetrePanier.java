76e646ef11fde945f5c932f4643775ec1af98114
Eliitt <eliott82000@gmail.com>
2024-06-04 15:10:51 +0200

    Panier Entrer valeur

--- src/main/java/ihm/FenetrePanier.java
+++ src/main/java/ihm/FenetrePanier.java
@@ -1,35 +1,29 @@
 package ihm;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import javax.swing.JLabel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import java.awt.GridLayout;
-import javax.swing.SwingConstants;
-import javax.swing.table.DefaultTableModel;
-
-import modele.*;
-
-import javax.swing.JComboBox;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.MatteBorder;
 import java.awt.Color;
+import java.awt.GridLayout;
+
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
+
+import modele.Article;
+import modele.Panier;
 
 public class FenetrePanier extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	
-	private int nombreElement=14;
+
+	private int nombreElement = 14;
 
 	/**
 	 * Launch the application.
@@ -38,102 +32,102 @@ public class FenetrePanier extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public FenetrePanier() {
-		setTitle("Ô fromage - Panier");
-		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 600, 500);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	public FenetrePanier(Panier panier) {
+		this.setTitle("Ô fromage - Panier");
+		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		this.setBounds(100, 100, 600, 500);
+		this.contentPane = new JPanel();
+		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
-		
+		this.setContentPane(this.contentPane);
+		this.contentPane.setLayout(new BorderLayout(0, 0));
+
 		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.SOUTH);
+		this.contentPane.add(panel, BorderLayout.SOUTH);
 		panel.setLayout(new GridLayout(0, 3, 0, 0));
-		
+
 		JPanel panel_6 = new JPanel();
-		panel_6.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		panel_6.setBorder(new MatteBorder(1, 1, 1, 1, new Color(0, 0, 0)));
 		panel.add(panel_6);
 		panel_6.setLayout(new GridLayout(3, 1, 0, 0));
-		
+
 		JLabel Transporteur = new JLabel("Transporteur");
 		panel_6.add(Transporteur);
-		
+
 		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Colissimo", "Chronorelais", "Chronofresh"}));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] { "Colissimo", "Chronorelais", "Chronofresh" }));
 		panel_6.add(comboBox);
-		
+
 		JLabel lblNewLabel_12 = new JLabel("Frais de port offert dès 120€ d'achat");
 		panel_6.add(lblNewLabel_12);
-		
+
 		JPanel panel_7 = new JPanel();
 		panel.add(panel_7);
-		
+
 		JPanel panel_2 = new JPanel();
 		panel.add(panel_2);
 		panel_2.setLayout(new GridLayout(2, 0, 0, 0));
-		
+
 		JPanel panel_3 = new JPanel();
-		panel_3.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		panel_3.setBorder(new MatteBorder(1, 1, 1, 1, new Color(0, 0, 0)));
 		panel_2.add(panel_3);
 		panel_3.setLayout(new GridLayout(4, 2, 0, 0));
-		
+
 		JLabel SousTotalHT = new JLabel("Sous total HT");
 		panel_3.add(SousTotalHT);
-		
+
 		JLabel PrixHT = new JLabel("126.00€");
 		PrixHT.setHorizontalAlignment(SwingConstants.TRAILING);
 		panel_3.add(PrixHT);
-		
+
 		JLabel TVA = new JLabel("TVA (20%)");
 		panel_3.add(TVA);
-		
+
 		JLabel PrixTVA = new JLabel("25.20€");
 		PrixTVA.setHorizontalAlignment(SwingConstants.TRAILING);
 		panel_3.add(PrixTVA);
-		
+
 		JLabel FraisDePort = new JLabel("Frais de port");
 		panel_3.add(FraisDePort);
-		
+
 		JLabel PrixFraisDePort = new JLabel("Gratuit");
 		PrixFraisDePort.setHorizontalAlignment(SwingConstants.TRAILING);
 		panel_3.add(PrixFraisDePort);
-		
+
 		JLabel TotalTTC = new JLabel("Total TTC");
 		panel_3.add(TotalTTC);
-		
+
 		JLabel PrixTTC = new JLabel("151.20€");
 		PrixTTC.setHorizontalAlignment(SwingConstants.TRAILING);
 		panel_3.add(PrixTTC);
-		
+
 		JPanel panel_4 = new JPanel();
 		panel_2.add(panel_4);
 		panel_4.setLayout(new GridLayout(0, 2, 0, 0));
-		
+
 		JButton Valider = new JButton("Valider le panier");
 		panel_4.add(Valider);
-		
+
 		JButton Vider = new JButton("Vider le panier");
 		panel_4.add(Vider);
-		
+
 		JPanel panel_1 = new JPanel();
-		contentPane.add(panel_1, BorderLayout.NORTH);
+		this.contentPane.add(panel_1, BorderLayout.NORTH);
 		panel_1.setLayout(new BorderLayout(0, 0));
-		
+
 		JLabel Recapitulatif = new JLabel("Récapitulatif");
 		Recapitulatif.setHorizontalAlignment(SwingConstants.LEFT);
 		panel_1.add(Recapitulatif);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
-		contentPane.add(scrollPane, BorderLayout.CENTER);
-		
+		this.contentPane.add(scrollPane, BorderLayout.CENTER);
+
 		JPanel ContenuPanier = new JPanel();
 		scrollPane.setViewportView(ContenuPanier);
-		ContenuPanier.setLayout(new GridLayout(nombreElement, 0, 0, 0));
-		
-		for(int i=0;i<nombreElement;i++) {
-			ligneTableau(ContenuPanier, article);
+		ContenuPanier.setLayout(new GridLayout(this.nombreElement, 0, 0, 0));
+
+		for (Article a : panier.getPanier().keySet()) {
+			this.ligneTableau(ContenuPanier, a);
 		}
 	}
 
@@ -141,30 +135,31 @@ public class FenetrePanier extends JFrame {
 		JPanel Element = new JPanel();
 		ContenuPanier.add(Element);
 		Element.setLayout(new GridLayout(0, 5, 0, 0));
-		
+
 		JLabel ImageElement = new JLabel(article.getFromage().getNomImage());
 		Element.add(ImageElement);
-		
+
 		JPanel TexteElement = new JPanel();
 		Element.add(TexteElement);
 		TexteElement.setLayout(new GridLayout(3, 0, 0, 0));
-		
+
 		JLabel NomElement = new JLabel(article.getFromage().getDésignation());
 		TexteElement.add(NomElement);
-		
+
 		JLabel PoidsElement = new JLabel("New label");
 		TexteElement.add(PoidsElement);
-		
+
 		JLabel PrixUnitElement = new JLabel(article.toStringPrix());
 		TexteElement.add(PrixUnitElement);
-		
+
 		JComboBox QuantiteElement = new JComboBox();
-		QuantiteElement.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "67", "8", "9", "7", "5", "1"}));
+		QuantiteElement.setModel(
+				new DefaultComboBoxModel(new String[] { "1", "2", "3", "4", "5", "67", "8", "9", "7", "5", "1" }));
 		Element.add(QuantiteElement);
-		
+
 		JLabel PrixTotalElement = new JLabel("New label");
 		Element.add(PrixTotalElement);
-		
+
 		JLabel SupprimerElement = new JLabel("New label");
 		Element.add(SupprimerElement);
 	}
--- src/main/java/ihm/ListeFromage.java
+++ src/main/java/ihm/ListeFromage.java
@@ -1,42 +1,47 @@
 package ihm;
+
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.JLabel;
 import javax.swing.SwingConstants;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import javax.swing.JCheckBox;
-import javax.swing.JButton;
+import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import java.util.ArrayList;
-import java.util.List;
-import modele.*;
+import javax.swing.table.DefaultTableModel;
+
+import modele.Fromages;
+import modele.GenerationFromages;
+import modele.Panier;
+import modele.TypeLait;
 
 public class ListeFromage extends JFrame {
 
-    private static final long serialVersionUID = 1L;
-    private JPanel contentPane;
-    private JTable table;
-    private Fromages listeFromages;
-    private JCheckBox[] checkboxs = new JCheckBox[3];
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+	private Fromages listeFromages;
+	private JCheckBox[] checkboxs = new JCheckBox[3];
 
-    /**
+	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				try {
 					ListeFromage frame = new ListeFromage();
@@ -48,32 +53,32 @@ public class ListeFromage extends JFrame {
 		});
 	}
 
-    /**
-     * Create the frame.
-     */
+	/**
+	 * Create the frame.
+	 */
 	public ListeFromage() {
-		setTitle("Ô fromage - Liste Fromages");
+		this.setTitle("Ô fromage - Liste Fromages");
 		this.listeFromages = GenerationFromages.générationBaseFromages();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 500);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(100, 100, 600, 500);
+		this.contentPane = new JPanel();
+		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		//Panel principal
-		this.setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
-				
+		// Panel principal
+		this.setContentPane(this.contentPane);
+		this.contentPane.setLayout(new BorderLayout(0, 0));
+
 		JPanel panel_Principal = new JPanel();
-		contentPane.add(panel_Principal, BorderLayout.WEST);
+		this.contentPane.add(panel_Principal, BorderLayout.WEST);
 		panel_Principal.setLayout(new GridLayout(4, 0, 0, 0));
-				
+
 		JLabel ImageFromage = new JLabel("");
 		panel_Principal.add(ImageFromage);
-				
+
 		JPanel Type_Fromage = new JPanel();
 		panel_Principal.add(Type_Fromage);
 		Type_Fromage.setLayout(new GridLayout(1, 3, 0, 0));
-				
+
 		JCheckBox checkbox_Chevre = new JCheckBox("Chévre");
 		Type_Fromage.add(checkbox_Chevre);
 		this.checkboxs[0] = checkbox_Chevre;
@@ -83,27 +88,27 @@ public class ListeFromage extends JFrame {
 		JCheckBox checkbox_Vache = new JCheckBox("Vache");
 		Type_Fromage.add(checkbox_Vache);
 		this.checkboxs[2] = checkbox_Vache;
-				
+
 		JButton boutonPanier = new JButton("Panier");
 		panel_Principal.add(boutonPanier);
 		boutonPanier.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-			FenetrePanier p = new FenetrePanier();
-			p.setVisible(true);
-		}
-	});
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				FenetrePanier p = new FenetrePanier(new Panier());
+				p.setVisible(true);
+			}
+		});
 		JLabel Livraison_Gratuite = new JLabel("Somme a payer avant livraison");
 		Livraison_Gratuite.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_Principal.add(Livraison_Gratuite);
-		
+
 		JPanel panel_1 = new JPanel();
-		contentPane.add(panel_1, BorderLayout.CENTER);
+		this.contentPane.add(panel_1, BorderLayout.CENTER);
 		panel_1.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel_2 = new JPanel();
 		panel_1.add(panel_2, BorderLayout.SOUTH);
-		
+
 		JButton BoutonQuitter = new JButton("Quitter");
 		BoutonQuitter.addActionListener(new ActionListener() {
 			@Override
@@ -112,65 +117,61 @@ public class ListeFromage extends JFrame {
 			}
 		});
 		panel_2.add(BoutonQuitter);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		panel_1.add(scrollPane, BorderLayout.CENTER);
-		
-		table = new JTable();
-		table.addMouseListener(new MouseAdapter() {
+
+		this.table = new JTable();
+		this.table.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				if (e.getClickCount() == 2 && !e.isConsumed()) {
-                    e.consume();
-                    System.out.println("Double Click");
-                    FicheProduit p = new FicheProduit();
-    				p.setVisible(true);
-    			}
+					e.consume();
+					System.out.println("Double Click");
+					FicheProduit p = new FicheProduit();
+					p.setVisible(true);
+				}
 			}
 		});
-		scrollPane.setViewportView(table);
-		setupCheckbox();
-		reload();
+		scrollPane.setViewportView(this.table);
+		this.setupCheckbox();
+		this.reload();
 		System.out.println("push");
 	}
 
-    private void reload() {
-        DefaultTableModel model = new DefaultTableModel() {
-            private static final long serialVersionUID = 1L;
+	private void reload() {
+		DefaultTableModel model = new DefaultTableModel() {
+			private static final long serialVersionUID = 1L;
 
-            @Override
-            public boolean isCellEditable(int row, int column) {
-                // all cells false
-                return false;
-            }
-        };
-        List<TypeLait> laits = new ArrayList<>();
-        if (this.checkboxs[0].isSelected()) {
-            laits.add(TypeLait.CHEVRE);
-        }
-        if (this.checkboxs[1].isSelected()) {
-            laits.add(TypeLait.BREBIS);
-        }
-        if (this.checkboxs[2].isSelected()) {
-            laits.add(TypeLait.VACHE);
-        }
-        model.setDataVector(
-                this.listeFromages.arrayFromages(laits),
-                new String[] {
-                        "Fromages"
-                });
-        this.table.setModel(model);
-    }
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				// all cells false
+				return false;
+			}
+		};
+		List<TypeLait> laits = new ArrayList<>();
+		if (this.checkboxs[0].isSelected()) {
+			laits.add(TypeLait.CHEVRE);
+		}
+		if (this.checkboxs[1].isSelected()) {
+			laits.add(TypeLait.BREBIS);
+		}
+		if (this.checkboxs[2].isSelected()) {
+			laits.add(TypeLait.VACHE);
+		}
+		model.setDataVector(this.listeFromages.arrayFromages(laits), new String[] { "Fromages" });
+		this.table.setModel(model);
+	}
 
-    private void setupCheckbox() {
-        for (JCheckBox c : this.checkboxs) {
-            c.setSelected(true);
-            c.addChangeListener(new ChangeListener() {
-                @Override
-                public void stateChanged(ChangeEvent e) {
-                    reload();
-                }
-            });
-        }
-    }
+	private void setupCheckbox() {
+		for (JCheckBox c : this.checkboxs) {
+			c.setSelected(true);
+			c.addChangeListener(new ChangeListener() {
+				@Override
+				public void stateChanged(ChangeEvent e) {
+					ListeFromage.this.reload();
+				}
+			});
+		}
+	}
 }
