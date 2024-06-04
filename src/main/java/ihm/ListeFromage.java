a47e374d9ee2c0853ab09a9d472333b5f6655828
Lagourgue Maxence <maxence.lagourgue@free.fr>
2024-06-04 15:13:17 +0200

    mise à jour de la liste de fromage

--- src/main/java/ihm/ListeFromage.java
+++ src/main/java/ihm/ListeFromage.java
@@ -1,47 +1,44 @@
 package ihm;
-
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.event.MouseMotionAdapter;
 
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JCheckBox;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import javax.swing.table.DefaultTableModel;
-
-import modele.Fromages;
-import modele.GenerationFromages;
-import modele.Panier;
-import modele.TypeLait;
+import java.util.ArrayList;
+import java.util.List;
+import modele.*;
 
 public class ListeFromage extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTable table;
-	private Fromages listeFromages;
-	private JCheckBox[] checkboxs = new JCheckBox[3];
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private JTable table;
+    private Fromages listeFromages;
+    private JCheckBox[] checkboxs = new JCheckBox[3];
+    private Panier panier = new Panier();
 
-	/**
+    /**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
-			@Override
 			public void run() {
 				try {
 					ListeFromage frame = new ListeFromage();
@@ -53,32 +50,32 @@ public class ListeFromage extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+    /**
+     * Create the frame.
+     */
 	public ListeFromage() {
-		this.setTitle("Ô fromage - Liste Fromages");
+		setTitle("Ô fromage - Liste Fromages");
 		this.listeFromages = GenerationFromages.générationBaseFromages();
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBounds(100, 100, 600, 500);
-		this.contentPane = new JPanel();
-		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		// Panel principal
-		this.setContentPane(this.contentPane);
-		this.contentPane.setLayout(new BorderLayout(0, 0));
-
+		//Panel principal
+		this.setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+				
 		JPanel panel_Principal = new JPanel();
-		this.contentPane.add(panel_Principal, BorderLayout.WEST);
+		contentPane.add(panel_Principal, BorderLayout.WEST);
 		panel_Principal.setLayout(new GridLayout(4, 0, 0, 0));
-
+				
 		JLabel ImageFromage = new JLabel("");
 		panel_Principal.add(ImageFromage);
-
+				
 		JPanel Type_Fromage = new JPanel();
 		panel_Principal.add(Type_Fromage);
 		Type_Fromage.setLayout(new GridLayout(1, 3, 0, 0));
-
+				
 		JCheckBox checkbox_Chevre = new JCheckBox("Chévre");
 		Type_Fromage.add(checkbox_Chevre);
 		this.checkboxs[0] = checkbox_Chevre;
@@ -88,27 +85,27 @@ public class ListeFromage extends JFrame {
 		JCheckBox checkbox_Vache = new JCheckBox("Vache");
 		Type_Fromage.add(checkbox_Vache);
 		this.checkboxs[2] = checkbox_Vache;
-
+				
 		JButton boutonPanier = new JButton("Panier");
 		panel_Principal.add(boutonPanier);
 		boutonPanier.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				FenetrePanier p = new FenetrePanier(new Panier());
-				p.setVisible(true);
-			}
-		});
-		JLabel Livraison_Gratuite = new JLabel("Somme a payer avant livraison");
+				@Override
+				public void actionPerformed(ActionEvent e) {
+			FenetrePanier p = new FenetrePanier( new Panier());
+			p.setVisible(true);
+		}
+	});
+		JLabel Livraison_Gratuite = new JLabel("Il manque " + this.panier.getPrix() + " euros avant la livraison gratuite");
 		Livraison_Gratuite.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_Principal.add(Livraison_Gratuite);
-
+		
 		JPanel panel_1 = new JPanel();
-		this.contentPane.add(panel_1, BorderLayout.CENTER);
+		contentPane.add(panel_1, BorderLayout.CENTER);
 		panel_1.setLayout(new BorderLayout(0, 0));
-
+		
 		JPanel panel_2 = new JPanel();
 		panel_1.add(panel_2, BorderLayout.SOUTH);
-
+		
 		JButton BoutonQuitter = new JButton("Quitter");
 		BoutonQuitter.addActionListener(new ActionListener() {
 			@Override
@@ -117,61 +114,66 @@ public class ListeFromage extends JFrame {
 			}
 		});
 		panel_2.add(BoutonQuitter);
-
+		
 		JScrollPane scrollPane = new JScrollPane();
 		panel_1.add(scrollPane, BorderLayout.CENTER);
-
-		this.table = new JTable();
-		this.table.addMouseListener(new MouseAdapter() {
+		
+		table = new JTable();
+		table.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
+				String img = listeFromages.getFromage(table.getValueAt(table.getSelectedRow(),0).toString()).getNomImage();
+				ImageFromage.setIcon(new ImageIcon("src/main/resources/images/fromages/hauteur40/" + img + ".jpg"));
 				if (e.getClickCount() == 2 && !e.isConsumed()) {
-					e.consume();
-					System.out.println("Double Click");
-					FicheProduit p = new FicheProduit();
-					p.setVisible(true);
-				}
+                    e.consume();
+                    System.out.println(table.getValueAt(table.getSelectedRow(), 0));
+                    FicheProduit p = new FicheProduit();
+    				p.setVisible(true);
+    			}
 			}
 		});
-		scrollPane.setViewportView(this.table);
-		this.setupCheckbox();
-		this.reload();
-		System.out.println("push");
+		scrollPane.setViewportView(table);
+		setupCheckbox();
+		reload();
 	}
 
-	private void reload() {
-		DefaultTableModel model = new DefaultTableModel() {
-			private static final long serialVersionUID = 1L;
+    private void reload() {
+        DefaultTableModel model = new DefaultTableModel() {
+            private static final long serialVersionUID = 1L;
 
-			@Override
-			public boolean isCellEditable(int row, int column) {
-				// all cells false
-				return false;
-			}
-		};
-		List<TypeLait> laits = new ArrayList<>();
-		if (this.checkboxs[0].isSelected()) {
-			laits.add(TypeLait.CHEVRE);
-		}
-		if (this.checkboxs[1].isSelected()) {
-			laits.add(TypeLait.BREBIS);
-		}
-		if (this.checkboxs[2].isSelected()) {
-			laits.add(TypeLait.VACHE);
-		}
-		model.setDataVector(this.listeFromages.arrayFromages(laits), new String[] { "Fromages" });
-		this.table.setModel(model);
-	}
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                // all cells false
+                return false;
+            }
+        };
+        List<TypeLait> laits = new ArrayList<>();
+        if (this.checkboxs[0].isSelected()) {
+            laits.add(TypeLait.CHEVRE);
+        }
+        if (this.checkboxs[1].isSelected()) {
+            laits.add(TypeLait.BREBIS);
+        }
+        if (this.checkboxs[2].isSelected()) {
+            laits.add(TypeLait.VACHE);
+        }
+        model.setDataVector(
+                this.listeFromages.arrayFromages(laits),
+                new String[] {
+                        "Fromages"
+                });
+        this.table.setModel(model);
+    }
 
-	private void setupCheckbox() {
-		for (JCheckBox c : this.checkboxs) {
-			c.setSelected(true);
-			c.addChangeListener(new ChangeListener() {
-				@Override
-				public void stateChanged(ChangeEvent e) {
-					ListeFromage.this.reload();
-				}
-			});
-		}
-	}
+    private void setupCheckbox() {
+        for (JCheckBox c : this.checkboxs) {
+            c.setSelected(true);
+            c.addChangeListener(new ChangeListener() {
+                @Override
+                public void stateChanged(ChangeEvent e) {
+                    reload();
+                }
+            });
+        }
+    }
 }
--- src/main/java/modele/Panier.java
+++ src/main/java/modele/Panier.java
@@ -48,5 +48,4 @@ public class Panier {
 		}
 		return prix;
 	}
-	
 }
